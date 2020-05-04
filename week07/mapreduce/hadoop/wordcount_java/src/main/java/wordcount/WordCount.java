7698489cce527fe45a18694afcc811c2c17541a1
joshua-dev <msh01175@gmail.com>
2020-05-04 12:42:30 +0900

    Created to jar: Upload to HDFS cluster

--- week07/mapreduce/hadoop/wordcount_java/src/main/java/wordcount/WordCount.java
+++ week07/mapreduce/hadoop/wordcount_java/src/main/java/wordcount/WordCount.java
@@ -5,13 +5,16 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-//import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class WordCount extends Configured implements Tool {
+
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new WordCount(), args);
+  }
 
   public int run(String[] strings) throws Exception {
     Job job = Job.getInstance(getConf());
@@ -23,9 +26,6 @@ public class WordCount extends Configured implements Tool {
 
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(IntWritable.class);
-
-//    job.setInputFormatClass(TextInputFormat.class);
-//    job.setOutputFormatClass(TextOutputFormat.class);
 
     FileInputFormat.addInputPath(job, new Path(strings[0]));
     FileOutputFormat.setOutputPath(job, new Path(strings[0] + ".out"));
--- week07/mapreduce/hadoop/wordcount_scala/src/main/scala/wordcount/WordCountReducer.scala
+++ week07/mapreduce/hadoop/wordcount_scala/src/main/scala/wordcount/WordCountReducer.scala
@@ -8,9 +8,11 @@ import org.apache.hadoop.mapreduce.Reducer
 import collection.JavaConverters._
 
 class WordCountReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
+  val count = new IntWritable()
 
-  override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit =
-    context.write(key, new IntWritable(values.asScala.foldLeft(0)(_ + _.get())))
-  // end method reduce.
+  override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
+    count.set(values.asScala.map(x => x.get()).sum)
+    context.write(key, count)
+  } // end method reduce.
 
 } // end class WordCountReducer
